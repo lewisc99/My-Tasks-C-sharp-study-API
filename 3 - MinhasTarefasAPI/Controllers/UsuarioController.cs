adfdfad488ec87e5d01db8c27a90d55a44140a53
lewisc99 <lewiscontato99@gmail.com>
2022-06-20 11:16:24 -0300

    53. [ESTUDO] UsuarioController - Login - Parte 2

--- 3 - MinhasTarefasAPI/Controllers/UsuarioController.cs
+++ 3 - MinhasTarefasAPI/Controllers/UsuarioController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace _3___MinhasTarefasAPI.Controllers
@@ -18,10 +19,12 @@ namespace _3___MinhasTarefasAPI.Controllers
 
         private readonly IUsuarioRepository _usuarioRepository;
         private readonly SignInManager<ApplicationUser> _signInManager;
-        public UsuarioController(IUsuarioRepository usuarioRepository, SignInManager<ApplicationUser> signInManager)
+        private readonly UserManager<ApplicationUser> _userManager;
+        public UsuarioController(IUsuarioRepository usuarioRepository, SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager)
         {
             _usuarioRepository = usuarioRepository;
             _signInManager = signInManager;
+            _userManager = userManager;
         }
 
 
@@ -52,6 +55,40 @@ namespace _3___MinhasTarefasAPI.Controllers
                 {
                     return NotFound("Usuario Não Localizado");
                 }
+            }
+            else
+            {
+                return UnprocessableEntity(ModelState);
+            }
+        }
+
+        public ActionResult Cadastrar([FromBody] UsuarioDTO usuarioDTO )
+        {
+            if (ModelState.IsValid)
+            {
+
+                ApplicationUser usuario = new ApplicationUser();
+                usuario.FullName = usuarioDTO.Nome;
+                usuario.Email = usuarioDTO.Email;
+               
+                var result = _userManager.CreateAsync(usuario, usuarioDTO.Senha).Result;
+
+
+                if (!result.Succeeded)
+                {
+                    List<string> errors = new List<string>();
+                    
+                    foreach(var error in result.Errors)
+                    {
+                        errors.Add(error.Description);
+                    }
+                    return UnprocessableEntity(errors);
+                }
+                else
+                {
+                    return Ok(usuario);
+                }
+
             }
             else
             {
